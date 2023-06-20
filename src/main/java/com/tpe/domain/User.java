9709b8d42ebb3848a6c83435e614fb7635e077fc
Muhammed Musab Demir <mmusabdemirr@gmail.com>
2023-06-20 22:49:24 +0300

    Initial commit

---
+++ src/main/java/com/tpe/domain/User.java
@@ -0,0 +1,41 @@
+package com.tpe.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "t_user") //Burada tablo ismini user olarak bıraktığımızda SQL hata veriyor, o yüzden değiştirmek zorundayız.
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 25, nullable = false)
+    private String firstName;
+
+    @Column(length = 25, nullable = false)
+    private String lastName;
+
+    @Column(length = 25, nullable = false, unique = true)
+    private String userName;
+
+    @Column(length = 255, nullable = false) //password DB ye kaydedilmeden once sifrelenecegi icin
+    private String password;                //karmasik ve uzun olacak
+
+    //role
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id")) //opsiyonel
+    private Set<Role> roles = new HashSet<>();
+
+}
